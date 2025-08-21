c7edbf32d6404538d907241e8f3523973b5a151c
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-21 13:04:48 +0000

    adf

--- github-actions/bazel/setup/setup-ci-bazelrc.mjs
+++ github-actions/bazel/setup/setup-ci-bazelrc.mjs
@@ -5,12 +5,12 @@ import fs from 'fs';
 import assert from 'node:assert';
 
 // followed characters that weren't supposed to be escaped.
-const BAZEL_REPO_CACHE = process.env['BAZEL_REPO_CACHE'];
-const BAZELRC_PATH = process.env['BAZELRC_PATH'];
-const escapedCachePath = BAZEL_REPO_CACHE.replace(/\\/g, '\\\\');
+const BAZEL_REPO_CACHE = process.argv[2];
+const BAZELRC_PATH = process.argv[3];
 
-assert(BAZEL_REPO_CACHE, 'BAZEL_REPO_CACHE environment variable is not defined.');
-assert(BAZELRC_PATH, 'BAZELRC_PATH environment variable is not defined.');
+// Check to ensure the arguments were provided
+assert(BAZEL_REPO_CACHE, 'BAZEL_REPO_CACHE argument is not defined.');
+assert(BAZELRC_PATH, 'BAZELRC_PATH argument is not defined.');
 
 const bazelRcContent = `
 # Print all the options that apply to the build.
